f4aa70e8a24febcdaf430c9a38c05690bf5f96d7
maaretp <maaret.pyhajarvi@gmail.com>
2025-02-09 20:33:24 +0200

    Add test to verify count

--- test_new_todo.py
+++ test_new_todo.py
@@ -10,3 +10,11 @@ def test_add_and_remove_todo(br_page: Page, input_text):
     br_page.locator("ul.todo-list").hover()
     br_page.locator(".destroy").click()
     expect(br_page.locator("ul.todo-list")).not_to_have_text(input_text)
+
+def test_add_five_todos_and_verify_count(br_page: Page):
+    for i in range(5):
+        br_page.fill("input.new-todo", f"todo {i+1}")
+        br_page.press("input.new-todo", "Enter")
+    expect(br_page.locator('.todo-count')).to_contain_text('5')
+
+
