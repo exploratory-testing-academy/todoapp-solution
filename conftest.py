ec339b8de55da3164f20f297820ddd3cc030ba2b
maaretp <maaret.pyhajarvi@gmail.com>
2025-02-09 20:00:42 +0200

    Add single test

---
+++ conftest.py
@@ -0,0 +1,43 @@
+import pytest
+from playwright.sync_api import Browser, Page, BrowserContext
+import os
+from slugify import slugify
+
+
+@pytest.fixture(scope="session", autouse=True)
+def url(pytestconfig):
+    url = None
+    if url is None:
+        url = pytestconfig.getoption("base_url")
+    yield url
+
+
+@pytest.fixture(scope="session")
+def br(browser: Browser, my_context_arguments, pytestconfig, request: pytest.FixtureRequest) -> BrowserContext:
+    new_context = browser.new_context(**my_context_arguments)
+    new_context.tracing.start(snapshots=True, screenshots=True)
+    yield new_context
+    new_context.tracing.stop(path=_build_result_folder(pytestconfig, request, "trace.zip"))
+    new_context.close()
+
+
+@pytest.fixture(scope="function")
+def br_page(br, url) -> Page:
+    new_page = br.new_page()
+    new_page.goto(url)
+    yield new_page
+
+
+@pytest.fixture(scope="session")
+def my_context_arguments(url):
+    context_args = {
+        "base_url": url,
+        "locale": "fi-FI",
+        "viewport": {"width": 1280, "height": 720},
+        "ignore_https_errors": True,
+        "java_script_enabled": True,
+    }
+    return context_args
+
+def _build_result_folder(pytestconfig, request: pytest.FixtureRequest, folder_or_file_name: str) -> str:
+    return os.path.join(pytestconfig.getoption("--output"), slugify(request.node.name), folder_or_file_name)
---
+++ test_new_todo.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import expect
+from playwright.sync_api import Page
+import pytest
+from data.data_generator import Data
+
+@pytest.mark.parametrize("input_text", ["milk"], ids=["one"])
+def test_new_todo(br_page: Page, input_text):
+    br_page.fill("input.new-todo", input_text)
+    br_page.press("input.new-todo", "Enter")
+    expect(br_page.locator("ul.todo-list")).to_have_text(input_text)
